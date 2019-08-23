eb774dfdd5c059b110bbc2f9efd58f8064446881
Trace <lukaszdabrowski881@gmail.com>
2019-08-23 13:03:33 +0200

    Added missing properties to Domain

--- TraceLd.PlotlySharp/TraceLd.PlotlySharp/Api/Domain.cs
+++ TraceLd.PlotlySharp/TraceLd.PlotlySharp/Api/Domain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
@@ -5,9 +6,42 @@ namespace TraceLd.PlotlySharp.Api
 {
     public class Domain
     {
+        private double? _column;
+        private double? _row;
+        
         [JsonProperty("x")]
         public List<double> X { get; set; }
         [JsonProperty("y")]
         public List<double> Y { get; set; }
+        
+        [JsonProperty("column")]
+        public double? Column
+        {
+            get => _column;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),"Column must be greater than or equal to 0");
+                }
+
+                _column = value;
+            }
+        }
+        
+        [JsonProperty("row")]
+        public double? Row
+        {
+            get => _row;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),"Row must be greater than or equal to 0");
+                }
+
+                _row = value;
+            }
+        }
     }
 }
